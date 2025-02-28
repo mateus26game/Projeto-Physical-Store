c637ea83c51c8844b20df17606ed956183ee61d3
mateus <mateus26guerra@gmail.com>
2025-02-28 20:52:24 -0300

    Salvando alterações locais

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import pool from "./database";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("teste de api 🚀");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Conexão com o banco de dados bem-sucedida! Hora atual: ${result.rows[0].now}`);
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+    res.status(500).send("Erro ao conectar ao banco de dados.");
+  }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
